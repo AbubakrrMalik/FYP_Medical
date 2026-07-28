5f4c2944e17cd3036e6e9cba4846932b78e1c1fc
AbubakrrMalik <168067807+AbubakrrMalik@users.noreply.github.com>
2026-07-28 14:38:41 +0500

    Update CORS settings and comment out PORT

--- GeneDrugCorr/Backend_fastapi/main.py
+++ GeneDrugCorr/Backend_fastapi/main.py
@@ -8,7 +8,7 @@ from pathlib import Path
 
 app = FastAPI()
 
-PORT = 5001
+# PORT = 5001
 
 # -------------------------
 # CORS
@@ -16,12 +16,13 @@ PORT = 5001
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
-    ],
-    allow_credentials=True,
-    allow_methods=["GET", "POST"],
+    # allow_origins=[
+    #     "http://localhost:5173",
+    #     "http://127.0.0.1:5173"
+    # ],
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
     allow_headers=["*"]
 )
 
